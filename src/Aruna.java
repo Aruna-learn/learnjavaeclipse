ba43bea4075eb8090014ad6e1ad3e93d87fc068a
Aruna Reddy tech <arunaammu9922@gmail.com>
2024-05-28 20:41:56 +0530

    learn

--- src/Aruna.java
+++ src/Aruna.java
@@ -1,7 +1,21 @@
 
 public class Aruna {
+	void run() {
+		System.out.println("njoy");
+	}
+	public class Chinni extends Aruna{
+		void run() {
+			System.out.println("cry");
+		}
+		
+	
 
 	public static void main(String[] args) {
+		Chinni c=new Aruna();
+		c.run();
+	}
+	}
+
 		
 		//while(true) {// while infinete loop
 
@@ -16,11 +30,11 @@ public class Aruna {
 //		String s="Aruna";
 //		
 //		String s1="wedith";
-		 String s=30+40+"Sachin"+50+50+"Aruna";  
+		 //String s=30+40+"Sachin"+50+50+"Aruna";  
 	//	String s3=s1.concat(s);
 		
 		
-		System.out.println(s);
+		//System.out.println(s);
 				
 		
 		
